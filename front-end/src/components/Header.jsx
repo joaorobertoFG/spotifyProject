9a2c5c0f56d0d1f61b245b5a5bf838a1aa64f9bb
joaorobertoFG <joao.robertofg@gmail.com>
2025-02-14 23:00:05 -0300

    social buttons

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import logoSpotify from "../assets/logo/spotify-logo-branco.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faGithubAlt, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -13,11 +15,20 @@ const Header = () => {
         </Link>
       </div>
       <div className="header-center">
-        <Link to="/">
-          <FontAwesomeIcon className="house" icon={faHouse} />
+
+        <Link to="https://www.linkedin.com/in/joão-roberto-1877bb257/">
+          <FontAwesomeIcon className="social" icon={faLinkedinIn} />
         </Link>
+
+        <Link to="https://github.com/joaorobertoFG">
+          <FontAwesomeIcon className="social" icon={faGithubAlt} />
+        </Link>
+
         <div className="header__search">
-          <FontAwesomeIcon className="header__search--lupa" icon={faMagnifyingGlass} />
+          <FontAwesomeIcon
+            className="header__search--lupa"
+            icon={faMagnifyingGlass}
+          />
           <input
             className="search"
             type="text"
